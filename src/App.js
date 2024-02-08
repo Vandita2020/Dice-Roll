7f46736e428fe5ed091ff119e2fb7b8163164ce9
Vandita2020 <vandita.vk20@gmail.com>
2024-02-08 11:22:57 -0800

    React app for dice roll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { useState } from 'react';
+class NameForm extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    console.log(data.get('name')); // Reference by form input's `name` tag
+
+    fetch('http://localhost:3000/', {
+      method: 'POST',
+      body: data,
+    });
+  }
+  render() {
+    return (
+      <div><h3> Ready to try your luck </h3>
+      <h1> Roll the dice </h1>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">Name</label>
+        <input id="username" name="username" type="text" />
+
+        <button>Submit</button>
+      </form></div>
+      
+    );
+  }
 }
 
-export default App;
+export default NameForm;
